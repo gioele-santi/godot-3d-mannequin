f0f45ededb0bfa458d0031b5b0d07589d9462b61
gioele-santi <gioele.santi@gmail.com>
2021-08-03 19:59:52 +0200

    Added throw action with charge

--- godot/src/Dungeon/PickHand.gd
+++ godot/src/Dungeon/PickHand.gd
@@ -4,6 +4,10 @@ class_name PickHand
 var skin: Mannequiny = null
 var pick_object: Pickable = null 
 var hand_ik: SkeletonIK
+
+var strength := 0.0
+export (float, 2.0, 10.0) var strength_speed = 5.0
+export (float, 5.0, 20.0) var max_strength := 10.0
 # non optimal, it should be in the same scene
 #later try moving ik inside and see if it works
 # then I will need some kind of easy setup, maybe IK needs to be tailored anyway
@@ -15,15 +19,28 @@ func _ready() -> void:
 #	add_to_group("picker")
 	hand_ik = skeleton.get_node('HandRIK')
 
+func _process(delta: float) -> void:
+	if pick_object:
+		if Input.is_action_pressed('fire'):
+			strength = min(max_strength, strength + delta * strength_speed)
+			print(strength)
+		elif Input.is_action_just_released('fire'):
+			hand_ik.stop()
+			var dir := global_transform.basis.z.normalized() * strength + Vector3(0,5,0)
+			pick_object.get_thrown(dir)
+			yield(get_tree().create_timer(1.0), "timeout")
+			pick_object = null
+			strength = 0.0
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('interact'):
 		print("interact")
-	if event.is_action_pressed('fire') and pick_object:
-		hand_ik.stop()
-		var dir := global_transform.basis.z
-		pick_object.get_thrown(dir.normalized()*5)
-		yield(get_tree().create_timer(1.0), "timeout")
-		pick_object = null
+#	if event.is_action_pressed('fire') and pick_object:
+#		hand_ik.stop()
+#		var dir := global_transform.basis.z.normalized() * strength + Vector3(0,5,0)
+#		pick_object.get_thrown(dir)
+#		yield(get_tree().create_timer(1.0), "timeout")
+#		pick_object = null
 
 #add Tween and store default (relative) position
 # add animations for one handed attacks (it could be a FSM per se)
