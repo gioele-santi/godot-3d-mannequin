9e92daf041a34825a9650c52575d8626da85749f
gioele-santi <gioele.santi@gmail.com>
2021-08-04 23:49:53 +0200

    Added new container node for pick hand
    Adapted script, trigger throw actions using animations.

--- godot/src/Dungeon/PickHand.gd
+++ godot/src/Dungeon/PickHand.gd
@@ -1,16 +1,20 @@
 extends Area
 class_name PickHand
 
+# I need a container where to move all other nodes. It should always keep the same loc/rot relative to player position to have better direction with throw
+
 var skin: Mannequiny = null
 var pick_object: Pickable = null 
-var hand_ik: SkeletonIK
+var hand_ik: SkeletonIK # I need some kind of easy setup, but it must always be child of skeleton
+
+onready var right_hand := $HandRight
+onready var anim_player := $AnimationPlayer
 
 var strength := 0.0
 export (float, 2.0, 10.0) var strength_speed = 5.0
 export (float, 5.0, 20.0) var max_strength := 10.0
-# non optimal, it should be in the same scene
-#later try moving ik inside and see if it works
-# then I will need some kind of easy setup, maybe IK needs to be tailored anyway
+
+# make it a  FSM, states will be IDLE, CHARGE (throw), MOVE (either walk or run), check how to manage attacks
 
 func _ready() -> void:
 	yield(owner, "ready")
@@ -22,34 +26,36 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if pick_object:
 		if Input.is_action_pressed('fire'):
+			# start strength, derive from movement speed
 			strength = min(max_strength, strength + delta * strength_speed)
-			print(strength)
 		elif Input.is_action_just_released('fire'):
-			hand_ik.stop()
-			var dir := global_transform.basis.z.normalized() * strength + Vector3(0,5,0)
-			pick_object.get_thrown(dir)
-			yield(get_tree().create_timer(1.0), "timeout")
-			pick_object = null
-			strength = 0.0
+			anim_player.play('throw')
+			
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('interact'):
 		print("interact")
-#	if event.is_action_pressed('fire') and pick_object:
-#		hand_ik.stop()
-#		var dir := global_transform.basis.z.normalized() * strength + Vector3(0,5,0)
-#		pick_object.get_thrown(dir)
-#		yield(get_tree().create_timer(1.0), "timeout")
-#		pick_object = null
+	if event.is_action_pressed('fire') and pick_object:
+		anim_player.play('throw_charge')
+
+func release_object() -> void:
+	var dir := global_transform.basis.z.normalized() * strength + Vector3(0,5,0)
+	pick_object.get_thrown(dir)
+	yield(get_tree().create_timer(1.0), "timeout")
+	pick_object = null 
+
+func reset_hand() -> void:
+	hand_ik.stop()
+	strength = 0.0
 
 #add Tween and store default (relative) position
 # add animations for one handed attacks (it could be a FSM per se)
 
-func _on_HandRight_body_entered(body: Node) -> void:
+func _on_PickHand_body_entered(body: Node) -> void:
 	if pick_object:
 		return
 	# add animation for picking
 	if body.is_in_group("pickable"):
-		body.get_picked_by(self)
+		body.get_picked_by(right_hand)
 		pick_object = body
 		hand_ik.start()
