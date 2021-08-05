10b6be28f960786cfa1ef4c5f685ddaeea363685
gioele-santi <gioele.santi@gmail.com>
2021-08-05 23:43:16 +0200

    Replace auto pick with explicit interaction enabled by proximity

--- godot/src/Dungeon/PickHand.gd
+++ godot/src/Dungeon/PickHand.gd
@@ -4,12 +4,15 @@ class_name PickHand
 # I need a container where to move all other nodes. It should always keep the same loc/rot relative to player position to have better direction with throw
 
 var skin: Mannequiny = null
-var pick_object: Pickable = null 
+var pickable_object: Pickable = null 
 var hand_ik: SkeletonIK # I need some kind of easy setup, but it must always be child of skeleton
 
 onready var right_hand := $HandRight
 onready var anim_player := $AnimationPlayer
 
+var picking := false # <- this should be a state
+
+# Throw
 var strength := 0.0
 export (float, 2.0, 10.0) var strength_speed = 5.0
 export (float, 5.0, 20.0) var max_strength := 10.0
@@ -24,7 +27,7 @@ func _ready() -> void:
 	hand_ik = skeleton.get_node('HandRIK')
 
 func _process(delta: float) -> void:
-	if pick_object:
+	if pickable_object:
 		if Input.is_action_pressed('fire'):
 			# start strength, derive from movement speed
 			strength = min(max_strength, strength + delta * strength_speed)
@@ -33,16 +36,20 @@ func _process(delta: float) -> void:
 			
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed('interact'):
-		print("interact")
-	if event.is_action_pressed('fire') and pick_object:
+	if event.is_action_pressed('interact') and pickable_object:
+		picking = true
+		pickable_object.get_picked_by(right_hand)
+		hand_ik.start()
+		
+	if event.is_action_pressed('fire') and pickable_object:
 		anim_player.play('throw_charge')
 
 func release_object() -> void:
 	var dir := global_transform.basis.z.normalized() * strength + Vector3(0,5,0)
-	pick_object.get_thrown(dir)
+	pickable_object.get_thrown(dir)
 	yield(get_tree().create_timer(1.0), "timeout")
-	pick_object = null 
+	pickable_object = null 
+	picking = false
 
 func reset_hand() -> void:
 	hand_ik.stop()
@@ -52,10 +59,15 @@ func reset_hand() -> void:
 # add animations for one handed attacks (it could be a FSM per se)
 
 func _on_PickHand_body_entered(body: Node) -> void:
-	if pick_object:
+	if picking:
 		return
 	# add animation for picking
 	if body.is_in_group("pickable"):
-		body.get_picked_by(right_hand)
-		pick_object = body
-		hand_ik.start()
+		print("Can pick")
+		pickable_object = body
+
+
+func _on_PickHand_body_exited(body: Node) -> void:
+	if body.is_in_group("pickable") and not picking:
+		print("Pickable exited")
+		pickable_object = null
